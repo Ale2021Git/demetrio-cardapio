5cadedc2c58c51c7f57496669264eae77a7d89a7
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-04-06 15:34:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,28 @@
 const CACHE_NAME = 'demetrio-v1';
 const assets = [
-  './',
-  './index.html',
-  './manifest.json',
-  './assets/utensilios-de-cozinha.png',
-  './assets/fundo-xadrez.png'
+    './',
+    './index.html',
+    './manifest.json',
+    './assets/utensilios-de-cozinha.png',
+    './assets/fundo-xadrez.png'
 ];
 
+// Instalação e Cache
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Cache aberto');
+            return cache.addAll(assets);
+        })
+    );
 });
 
+// Responde offline
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
 
