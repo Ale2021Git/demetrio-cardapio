37b67416477c2cb21565b40f820778125313c7c9
Ale2021Git <86354193+Ale2021Git@users.noreply.github.com>
2026-04-08 07:14:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,53 @@
-Asset not available
+const CACHE_NAME = 'demetrio-v2'; // Mudei para v2 aqui!
+
+const assets = [
+    './',
+    './index.html',
+    './manifest.json',
+    './assets/churrasco.png',
+    './assets/carne-assada.png',
+    './assets/carne-ensopada.png',
+    './assets/carre.png',
+    './assets/dobradinha.png',
+    './assets/frango-assado.png',
+    './assets/frango-grelhado.png',
+    './assets/frango-milanesa.png',
+    './assets/strogonoff.png',
+    './assets/lingua.png',
+    './assets/linguica.png',
+    './assets/coca.png',
+    './assets/guarana.png',
+    './assets/guaravita.png',
+    './assets/fundo-xadrez.png',
+    './assets/utensilios-de-cozinha.png'
+];
+
+// Instalação e Cache
+self.addEventListener('install', event => {
+    self.skipWaiting(); // Força o novo SW a assumir o controle imediatamente
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    );
+});
+
+// Limpeza de cache antigo
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache); // Apaga o v1 antigo
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(res => res || fetch(event.request))
+    );
+});
+
